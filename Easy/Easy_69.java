ecca1a7af5d969cfd552a4659a2f82281a2badc1
DhruvilKhunt3 <dhruvilkhunt3@gmail.com>
2025-03-18 22:51:49 +0530

    March

---
+++ Easy/Easy_69.java
@@ -0,0 +1,13 @@
+package Leetcode_series.Easy;
+class Solution_E69 {
+    public int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+}
+class Easy69{
+    public static void main(String[] args) {
+        Solution_E69 s = new Solution_E69();
+        int x = 4;
+        s.mySqrt(x);
+    }
+}
